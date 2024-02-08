87206747ff196a2622208234d3e04d19958526b7
umumararungu <umcynthi@gmail.com>
2024-02-08 09:11:31 +0200

    created

--- objects.js
+++ objects.js
@@ -10,7 +10,7 @@ addtask('java')
 addtask('c')
 addtask('c++')
 
-console.log('Added Task \n----------------\n'+task_list)
+console.log('Added Task \n----------------\n'+task_list.join("\n "))
 
 // console.log(task_list)
 // const remove_task = task_list.pop()
@@ -18,10 +18,10 @@ console.log('Added Task \n----------------\n'+task_list)
 function removetask(){
     return task_list.splice(1,2)
 }
-console.log('Completed Task \n----------------\n'+removetask())
+console.log('Completed Task \n----------------\n'+removetask().join("\n "))
 
  function sorting(){
     return task_list.toSorted()
  }
 //  sorting()
- console.log('Sorted Task \n----------------\n'+sorting())
+ console.log('Sorted Task \n----------------\n'+sorting().join("\n "))
